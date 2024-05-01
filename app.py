b0834745ab25f86c6fa7b44c61d3e257a4413dc4
Joao Victor <joaovictorxl120@gmail.com>
2024-05-01 20:55:54 -0300

    estrutura basica

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
-from flask import Flask, jsonify
-from flask_cors import CORS # Compartilhamento de recursos
-
+from flask import Flask, render_template
+from flask import request, redirect, url_for 
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('index.html')
+
+@app.route('/chat')
+def chat():
+    username = request.args.get('username')
+    room = request.args.get('room')
+
+    if username and room:
+        return render_template('chat.html', username=username, room=room)
+    else:
+        return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
