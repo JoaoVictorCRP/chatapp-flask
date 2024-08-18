53ac5a5a85cadba5b385a8aec10980315287c656
JoaoVictorCRP <joaovictorxl120@gmail.com>
2024-08-17 21:41:34 -0300

    refactor | db.py | parametrized queries prevents sql injection

--- db.py
+++ db.py
@@ -9,37 +9,31 @@ cursor = con.cursor()
 # Save user to DB
 def save_user(user:User):
     password_hash = generate_password_hash(user.password)
-    cursor.execute(f"""
-        INSERT INTO users(username, email, password) VALUES
-            ('{user.username}', '{user.email}', '{password_hash}')
-        """
-    )
+    cursor.execute("""
+        INSERT INTO users(username, email, password) VALUES (?, ?, ?)
+    """, (user.username, user.email, password_hash))
     con.commit()
 
 # Get user data
 def get_user(username:str):
     """Queries userdata of a given user, searching by the username.
 
-        ~> Returns the queried user object
+        ~> Returns the queried user object (or None if the user does not exist)
     """
-    user_data = cursor.execute(f'SELECT * FROM users WHERE username="{username}"')
-    user_data = user_data.fetchone() 
-
-    # FIXME: MAYBE I COULD JUST ADD ALL VERIFICATION STEP RIGHT HERE
+    cursor.execute('SELECT username, email, password FROM users WHERE username = ?', (username, ))
+    user_data = cursor.fetchone() 
 
     return User(user_data[0],user_data[1], user_data[2]) if user_data else None
 
 # Checks if user data is already on the database
 def user_exists(**kwargs):
+
     # Checks if email already exists in database
-    
-    if kwargs.get('email'):
-        user_data = cursor.execute(f'SELECT * FROM users WHERE email="{kwargs.get('email')}"')
-        user_data = user_data.fetchone()
-        return True if user_data else False
+    if 'email' in kwargs:
+        cursor.execute('SELECT * FROM users WHERE email = ?', (kwargs['email'], ))
+        return cursor.fetchone() is not None
     
     # Checks if username is already taken
-    if kwargs.get('username'):
-        user_data = cursor.execute(f'SELECT * FROM users WHERE username="{kwargs.get('username')}"')
-        user_data = user_data.fetchone()
-        return True if user_data else False
+    if 'username' in kwargs:
+        cursor.execute("SELECT * FROM users WHERE username= ?", (kwargs['username'], ))
+        return cursor.fetchone() is not None
